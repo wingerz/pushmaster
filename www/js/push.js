31a64ba0551f023a9a5debd74bcdcf7bdd268578
Jeremy Latt <jlatt@sff7u125.706.yelpcorp.com>
2009-12-01 15:04:53 -0800

    don't use reload, hope for caching

--- www/js/push.js
+++ www/js/push.js
@@ -2,6 +2,6 @@ var PAGE_RELOAD_DELAY = 60000; // ms
 
 if (push.state != 'live') {
     setTimeout(function() {
-        location.reload();
+        location.href = location.href;
     }, PAGE_RELOAD_DELAY);
 }
