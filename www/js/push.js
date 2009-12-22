0abf88248b2e5f5497204991ac1778a2e76dad37
Jeremy Latt <jlatt@yelp.com>
2009-12-22 09:41:15 -0800

    don't reload when user has new request form up

--- www/js/push.js
+++ www/js/push.js
@@ -1,7 +1,21 @@
 var PAGE_RELOAD_DELAY = 60000; // ms
 
-if (push.state != 'live') {
+var canReload = function() {
+    return $('form.request div.content:visible').length == 0;
+};
+
+var maybeReloadAfterDelay = function() {
     setTimeout(function() {
-        location.href = location.href;
+        if (canReload()) {
+            location.href = location.href;
+        } else {
+            maybeReloadAfterDelay();
+        }
     }, PAGE_RELOAD_DELAY);
-}
+};
+
+$(function() {
+    if (push.state != 'live') {
+        maybeReloadAfterDelay();
+    }
+});
