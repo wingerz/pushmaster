fd7a7c7a45ca885aa68b058b541b46f73e08aefd
Jeremy Latt <jlatt@yelp.com>
2010-02-05 11:17:24 -0800

    can't require it when changing

--- pushmaster/model.py
+++ pushmaster/model.py
@@ -77,7 +77,7 @@ class Request(ROTModel):
     push_plans = db.BooleanProperty(default=False)
     no_testing = db.BooleanProperty(default=False)
     urgent = db.BooleanProperty(default=False)
-    target_date = db.DateProperty(required=True)
+    target_date = db.DateProperty()
 
     @property
     def uri(self):
