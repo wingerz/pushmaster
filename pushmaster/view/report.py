7cce4f9369e468af0d4aad630c8472ae5f37d3fc
Jeremy Latt <jlatt@yelp.com>
2010-02-23 11:42:09 -0800

    display dates in tz

--- pushmaster/view/report.py
+++ pushmaster/view/report.py
@@ -54,7 +54,7 @@ class ViewReport(RequestHandler):
         pushes = model.Push.for_week_of(from_date)
         for push in pushes:
             pushdiv = T.div(class_='push')
-            pushdiv(T.h2(push.ctime.strftime('%a, %l:%M %p, %e %b %Y')))
+            pushdiv(T.h2(T.a(href=push.uri)(push.ctime.astimezone(config.timezone).strftime('%a, %l:%M %p, %e %b %Y'))))
             reqlist = T.ol(class_='requests')
             for request in push.requests:
                 reqlist(common.request_item(request))
