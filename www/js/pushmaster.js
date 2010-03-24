917897655f6dfb2b6557727e187217b17759b244
Jeremy Latt <jlatt@yelp.com>
2010-03-24 11:45:14 -0700

    fix bookmarklet

--- www/js/pushmaster.js
+++ www/js/pushmaster.js
@@ -1,36 +1,102 @@
-$('a.toggle').live('click', function(e) {
-    e.preventDefault();
-    $(this)
-        .closest('form')
-        .find('div.content')
-        .slideToggle('fast');
-});
+this.provide = function() {
+    var namespace = this;
+    Array.forEach(arguments, function(name) {
+        if (!namespace[name]) {
+            namespace[name] = {};
+        }
+        namespace = namespace[name];
+        namespace.provide = this.provide;
+    }, this);
+};
 
-$('#new-request').live('click', function(e) {
-    var form = $('#new-request-form');
-    if (!form.data('newFormDialog')) {
-        form.dialog({
-            'title': 'Make Request',
-            'width': 700,
-            'height': 450
+provide('pushmaster', 'location');
+
+pushmaster.location.queryToObject = function(query) {
+    var object = {};
+    if (query) {
+        if (query.charAt(0) == '?') {
+            query = query.substring(1);
+        }
+        query.split('&').forEach(function(pair) {
+            var parts = pair.split('=').map(function(part) {
+                return decodeURIComponent(part.replace(/\+/g, '%20'));
+            });
+            object[parts[0]] = parts[1];
         });
-        form.data('newFormDialog', true);
-    } else {
-        if (form.dialog('isOpen')) {
-            form.dialog('close');
-        } else {
-            form.dialog('open');
+    }
+    return object;
+};
+
+provide('pushmaster', 'dialog');
+
+pushmaster.dialog.MakeRequest = function() {
+    this.form = $('#new-request-form');
+
+    if (location.search) {
+        var query = pushmaster.location.queryToObject(location.search);
+
+        if (query.subject) {
+            this.form.find('[name=subject]').val(query.subject);
+        }
+
+        if (query.message) {
+            this.form.find('[name=message]').val(query.message);
+        }
+
+        if (query.message || query.subject) {
+            this.open();
         }
     }
-});
+};
+pushmaster.dialog.MakeRequest.prototype = {
+    initialized: false,
+
+    dialogOptions: {
+        'title': 'Make Request',
+        'width': 700,
+        'height': 450
+    },
+
+    ensureDialog: function() {
+        if (!this.initialized) {
+            this.form.dialog(this.dialogOptions);
+            this.initialized = true;
+        }
+    },
+
+    open: function() {
+        this.ensureDialog();
+        if (!this.form.dialog('isOpen')) {
+            this.form.dialog('open');
+        }
+    },
+
+    close: function() {
+        if (this.initialized && this.form.dialog('isOpen')) {
+            this.form.dialog('close');
+        }
+    },
+
+    toggle: function() {
+        if (this.initialized && this.form.dialog('isOpen')) {
+            this.close();
+        } else {
+            this.open();
+        }
+    }
+};
+
+pushmaster.provide('page');
 
 $(function() {
-    // for CSS
-    if ($.browser.webkit) {
-        $('body').addClass('webkit');
-    }
+    pushmaster.page.makeRequest = new pushmaster.dialog.MakeRequest();
+
+    $('#new-request').click(function(e) {
+        pushmaster.page.makeRequest.toggle();
+    });
 });
 
+// initialize all datepickers
 $(function() {
     $('input.date').datepicker({ 
         'showAnim': 'fadeIn',
