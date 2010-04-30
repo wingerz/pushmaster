6f77095392d4e603058c1b70bc74c5cf8e60ac1a
Jeremy Latt <jlatt@yelp.com>
2010-04-30 15:45:34 -0700

    Revert "ajaxy push reload"
    This reverts commit 9c160c3e9bf2488c20584251259c9c8c16380c89.

--- www/js/push.js
+++ www/js/push.js
@@ -1,19 +1,7 @@
-var PAGE_RELOAD_DELAY = 30000; // ms
-
-var reloadPush = function() {
-    setTimeout(function() {
-        $.getJSON(location.href, function(data) {
-            $('#requests')
-                .empty()
-                .append($(data.requests).children());
-            $('#pending-requests')
-                .empty()
-                .append($(data.pending).children());
-            reloadPush();
-        });
-    }, PAGE_RELOAD_DELAY);
-};
+var PAGE_RELOAD_DELAY = 60000; // ms
 
 if (push.state != 'live') {
-    reloadPush();
+    setTimeout(function() {
+        location.href = location.href;
+    }, PAGE_RELOAD_DELAY);
 }
