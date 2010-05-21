50a0fe521611e2def848e0858306ae865a277cfb
Jeremy Latt <jlatt@yelp.com>
2010-05-20 18:56:28 -0700

    fix typo in push js that kept it from ajax-reloading

--- www/js/push.js
+++ www/js/push.js
@@ -1,21 +1,20 @@
-var PAGE_RELOAD_DELAY = 30 * 1000; // ms
+var PAGE_RELOAD_DELAY = 20 * 1000; // ms
 
 pushmaster.provide('push');
 
 pushmaster.push.retrievePushData = function() {
     if (push.state !== 'live') {
-        $.ajax({'url': '/push/' + push.key, 'contentType': 'application/json', 'success': pushmaster.push.loadAsyncPushData});
+        pushmaster.push.retrieveTimeout = setTimeout(function() {
+            pushmaster.push.retrieveTimeout = null;
+            $.ajax({'url': '/push/' + push.key, 'contentType': 'application/json', 'success': pushmaster.push.loadPushData});
+        }, PAGE_RELOAD_DELAY);
     }
 };
 
 pushmaster.push.loadPushData = function(pushData) {
     push.state = pushData.push.state;
     $('.push').html(pushData.html);
-    pushmaster.push.retrieveTimeout = setTimeout(pushmaster.push.retrievePushData, PAGE_RELOAD_DELAY);
+    pushmaster.push.retrievePushData();
 };
 
-pushmaster.push.load = function() {
-    pushmaster.push.retrieveTimeout = setTimeout(pushmaster.push.retrievePushData, PAGE_RELOAD_DELAY);
-};
-
-$(pushmaster.push.load);
+$(pushmaster.push.retrievePushData);
