395e69ed1108c7e8c26181b9b9780fcfcabf47f8
Jeremy Latt <jlatt@yelp.com>
2010-06-16 15:11:00 -0700

    bookmarklet pulls branch from code review

--- www/js/bookmarklet.js
+++ www/js/bookmarklet.js
@@ -1,28 +1,36 @@
 (function() {
+    var elementToText = function(i, a) {
+        return $(a).text(); 
+    };
+
+    var ticketNumberToURL = function(bug) { 
+        return 'https://trac.yelpcorp.com/ticket/' + bug.match(/\d+/)[0];
+    };
+
     var summary = $('#summary').text();
-
-    var codeReview = location.href;
-    var reviewers = $.makeArray(
-        $('#target_people a')
-            .map(function(i, a) {
-                return $(a).text();
-            }))
-        .join(', ');
-
-    var tickets = $('#bugs_closed')
-        .text()
-        .split(',')
-        .filter(Boolean)
-        .map(function(bug) { return 'https://trac.yelpcorp.com/ticket/' + bug.match(/\d+/)[0]; })
-        .join(', ');
+    var codeReview = location.href.split('#')[0];
+    var reviewers = $.makeArray($('#target_people a').map(elementToText));
+    var tickets = $('#bugs_closed').text().split(',').filter(Boolean).map(ticketNumberToURL);
+    var branch = $('#branch').text();
     
-    var message = [
-        codeReview, ' by ', reviewers, '\n\n',
-        'Tickets: ', tickets
-    ].join('');
+    var message = [];
+    if (reviewers.length > 0) {
+        message.push(codeReview, ' by ', reviewers.join(', '));
+    }
+    if (tickets.length > 0) {
+        if (message.length > 0) {
+            message.push('\n\n');
+        }
+        message.push(tickets.length == 1 ? 'Ticket: ' : 'Tickets: ', tickets.join(', '));
+    }
+    if (branch) {
+        if (message.length > 0) {
+            message.push('\n\n');
+        }
+        message.push('Branch: ', branch);
+    }
 
-    location.href = 'http://yelp-pushmaster.appspot.com/requests?' + $.param({
-        'subject': summary,
-        'message': message
-    });
+    message = message.join('');
+
+    location.href = 'http://yelp-pushmaster.appspot.com/requests?' + $.param({'subject': summary, 'message': message});
 })();
