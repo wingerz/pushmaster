28219b9a2c2fa60e234cad5539fd512eae31ea82
Jeremy Latt <jlatt@yelp.com>
2010-07-06 15:11:53 -0700

    pull reviewers from ship-it reviews

--- www/js/bookmarklet.js
+++ www/js/bookmarklet.js
@@ -9,7 +9,7 @@
 
     var summary = $('#summary').text();
     var codeReview = location.href.split('#')[0];
-    var reviewers = $.makeArray($('#target_people a').map(elementToText));
+    var reviewers = $('div.shipit').closest('div[id^=review]').find('a[href^=/users/]').map(elementToText).get();
     var tickets = $('#bugs_closed').text().split(',').filter(Boolean).map(ticketNumberToURL);
     var branch = $('#branch').text();
     
